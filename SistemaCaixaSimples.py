3bf861ae8cc385cd4e2ca63c74b26dec0d36f53c
Daniel Cordeiro <danielthug1402@gmail.com>
2025-10-22 17:26:48 -0400

    Primeira versão do Sistema de Caixa

---
+++ SistemaCaixaSimples.py
@@ -0,0 +1,33 @@
+#SIstema de Caixa Simples
+print("Bem Vindo à Loja DaniVerso")
+
+produtos = []
+total = 0
+while True:
+    #Inserir produto ou terminar progama
+    product = input("Enter the product name (or 'sair' to finish): ")
+    
+    if product.lower() == "sair":
+        print("Thank you for preference!")
+        break
+    #Inserir preço
+    price = float(input("Enter the price: "))
+    #Lista de produto e preço
+    print("Product Purchased:\n"
+          f"{product} - R${price:.2f}")
+    print("Product Added!\n")
+
+    #Guarda nome e preço
+    produtos.append((product, price))
+    total += price
+
+print("\n === Purchase Summary ===")
+#Mostra os itens dentro da lista
+for item in produtos:
+    print(f"{item[0]} - R${item[1]:.2f}")
+#total
+print(f"Total:  R${total:.2f}")
+#Produtos adicionados ao final
+print(f"{len(produtos)} products added.")
+
+
---
+++ treino.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number 1: "))
+num2 = int(input("Enter a number 2: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
